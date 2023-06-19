2ca06d407d41a2231b1668f06b8847608fd3a64b
PJ Manning <phil.manning11@gmail.com>
2023-06-20 00:52:38 +0800

    adding prettier plugin tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@vueuse/nuxt', '@pinia/nuxt', '@nuxthq/ui'],
   runtimeConfig: {
     public: {
       tmdbKey: process.env.NUXT_TMDB_KEY,
       tmdbBaseURL: process.env.NUXT_TMDB_BASE_URL,
     },
   },
-  devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@pinia/nuxt', '@nuxthq/ui'],
 })
