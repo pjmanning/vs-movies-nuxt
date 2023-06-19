22e764d28a63c1ca92840166a0cb7e3761099798
PJ Manning <phil.manning11@gmail.com>
2023-06-19 16:29:35 +0800

    adding vueuse, tmdb keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      tmdbKey: process.env.NUXT_TMDB_KEY,
+      tmdbBaseURL: process.env.NUXT_TMDB_BASE_URL,
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
 })
